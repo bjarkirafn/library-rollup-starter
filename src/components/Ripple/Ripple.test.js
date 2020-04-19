91efc1f5280c21d7f38ae8cdffc933eae3a2179c
bjarkirafn <bjarkirafn@gmail.com>
2020-04-19 04:14:25 +0000

    jest added

---
+++ src/components/Ripple/Ripple.test.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { Ripple } from '../..'
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Ripple/>)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
